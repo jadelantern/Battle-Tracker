8f4c0979160d1da96631923a157d86b37783336b
Jadelantern <natelee00@gmail.com>
2021-10-05 19:01:42 -0400

    Started adding weapon and army classes.

---
+++ BattleTracker/Troop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleTracker
+{
+    class Troop
+    {
+        public string name;
+        public int movement;
+        public int weaponSkill;
+        public int ballisticSkill;
+        public int strength;
+        public int toughness;
+        public int wounds;
+        public int numberOfAttacks;
+        public int leadership;
+        public int savingThrow;
+        public Weapon weapon;
+    }
+}
